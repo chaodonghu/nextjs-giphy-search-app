bd6dd26d995625dfe7f313b2368896a16ad4e08f
chaodonghu <dojostan@gmail.com>
2021-08-05 14:04:00 -0400

    add initial load of giphys and search bar

--- pages/about.jsx
+++ pages/about.jsx
@@ -1,6 +1,28 @@
 import Head from "next/head";
+import { useEffect } from "react";
 
-const About = () => {
+export async function getStaticProps() {
+  let catGiphys = await fetch(
+    "https://api.giphy.com/v1/gifs/search?q=memes&api_key=MBIWS7egmUvQexSSBnZHyvytFdKcsx7l&limit=10"
+  );
+  catGiphys = await catGiphys.json();
+  return { props: { catGiphys: catGiphys } };
+}
+const About = (props) => {
+  useEffect(() => {
+    console.log("props", props);
+  });
+
+  const handleInputs = (event) => {
+    console.log(event.target.value);
+    console.log(event.target.name);
+  };
+
+  const search = (event) => {
+    event.preventDefault();
+    console.log(formInputs.searchTerm);
+  };
+
   return (
     <div className="container">
       <Head>
@@ -8,34 +30,21 @@ const About = () => {
         <link rel="icon" href="/favicon.ico" />
         <link rel="stylesheet" href="/styles.css" />
       </Head>
-      <h1>About</h1>
-      <p>
-        Love giphys? So do we. use our app <b>giphy search</b> to find the
-        perfect giphy for any occasion.
-      </p>
+      <h1>Giphy Search App</h1>
 
-      <h2>Why do people love giphys?</h2>
+      <form onSubmit={search}>
+        <input name="searchTerm" type="text" onChange={handleInputs} required />
+        <button>Search</button>
+      </form>
 
-      <p>
-        Some people may work better with words, others with numbers, but
-        everyone gets pictures. 90% of information transmitted to the human
-        brain is visual.
-      </p>
-
-      <p>
-        The old saying "a picture is worth a thousand words" is quite cliche.
-        But that doesn't make it any less true, especially in marketing and
-        particularly in the instant-gratification, short attention span world we
-        live in today. Getting folks to retain (or even register) your messages
-        and content or take action is harder than ever, especially if all you
-        are giving them is words.
-      </p>
-
-      <p>
-        Images are stronger than words. However, the fast-moving nature of GIFs
-        make them stronger than images and their shorter length make them more
-        digestible than video. That's the short answer.
-      </p>
+      {props.catGiphys.data.map((each, index) => {
+        return (
+          <div key={each.id} className="giphy-search-results-grid">
+            <h3>{each.title}</h3>
+            <img src={each.images.original.url} alt={each.title} />
+          </div>
+        );
+      })}
     </div>
   );
 };
