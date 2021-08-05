d52fe1e6ca0b9459dfad991c38f17f3c5de8e931
chaodonghu <dojostan@gmail.com>
2021-08-05 14:23:44 -0400

    add dynamic routing of search term

--- pages/about.jsx
+++ pages/about.jsx
@@ -1,21 +1,14 @@
 import Head from "next/head";
 import { useEffect, useState } from "react";
 
-export async function getStaticProps() {
-  let catGiphys = await fetch(
-    "https://api.giphy.com/v1/gifs/search?q=memes&api_key=MBIWS7egmUvQexSSBnZHyvytFdKcsx7l&limit=10"
-  );
-  catGiphys = await catGiphys.json();
-  return { props: { catGiphys: catGiphys } };
-}
-const About = (props) => {
+export default function Home(initialData) {
   const [formInputs, setFormInputs] = useState();
-  const [searchResults, setSearchResults] = useState([]);
   const [searchTerm, setSearchTerm] = useState("cats");
+  const [searchResults, setSearchResults] = useState([]);
 
   useEffect(() => {
-    setSearchResults(props.catGiphys.data);
-  }, [props]);
+    setSearchResults(initialData.catGiphys.data);
+  }, [initialData]);
 
   const handleInputs = (event) => {
     let { name, value } = event.target;
@@ -25,7 +18,7 @@ const About = (props) => {
   const search = async (event) => {
     event.preventDefault();
     let giphys = await fetch(
-      `https://api.giphy.com/v1/gifs/search?q=${searchTerm}&api_key=MBIWS7egmUvQexSSBnZHyvytFdKcsx7l&limit=10`
+      `https://api.giphy.com/v1/gifs/search?q=${formInputs.searchTerm}&api_key=nPJNlVceWHERWCSDBW5XMo1p90l7l9ie&limit=6`
     );
     giphys = await giphys.json();
     setSearchResults(giphys.data);
@@ -39,24 +32,34 @@ const About = (props) => {
         <link rel="icon" href="/favicon.ico" />
         <link rel="stylesheet" href="/styles.css" />
       </Head>
+
       <h1>Giphy Search App</h1>
 
       <form onSubmit={search}>
-        <input name="searchTerm" type="text" onChange={handleInputs} required />
+        <input name="searchTerm" onChange={handleInputs} type="text" required />
         <button>Search</button>
       </form>
 
       <h1>Search results for: {searchTerm}</h1>
-      {searchResults.map((each, index) => {
-        return (
-          <div key={each.id} className="giphy-search-results-grid">
-            <h3>{each.title}</h3>
-            <img src={each.images.original.url} alt={each.title} />
-          </div>
-        );
-      })}
+
+      <div className="giphy-search-results-grid">
+        {searchResults.map((each, index) => {
+          return (
+            <div key={index}>
+              <h3>{each.title}</h3>
+              <img src={each.images.original.url} alt={each.title} />
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
-};
+}
 
-export default About;
+export async function getStaticProps() {
+  let catGiphys = await fetch(
+    "https://api.giphy.com/v1/gifs/search?q=cats&api_key=nPJNlVceWHERWCSDBW5XMo1p90l7l9ie&limit=6"
+  );
+  catGiphys = await catGiphys.json();
+  return { props: { catGiphys: catGiphys } };
+}
